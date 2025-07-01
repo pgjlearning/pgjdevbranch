39d9830b08d9cdc0a13539167139b0c481569c9e
Prafull Jagtap <prafullgovardhanjagtap@gmail.com>
2025-07-01 11:14:04 -0400

    initial commit

---
+++ force-app/main/default/aura/BoatSearchForm/BoatSearchFormHelper.js
@@ -0,0 +1,15 @@
+({
+    LoadBoatTypes : function(component,event) {
+        var action=component.get("c.getBoatType");
+        action.setCallback(this,function(response) {
+            var state= response.getState();
+            if(state==='SUCCESS'){
+                component.set("v.BoatTypes",response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);  
+    },
+})
